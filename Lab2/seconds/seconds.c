69e28fd8edb92cba54cbd2d606df5fa475dd06d0
ridasafeer <ridasafeer@3SH3.home>
2026-02-01 13:14:09 -0500

    refined with comments, linux distro information

--- Lab2/seconds/seconds.c
+++ Lab2/seconds/seconds.c
@@ -1,12 +1,11 @@
 /**
- * hello.c
+ * seconds.c
  *
  * Kernel module that communicates with /proc file system.
  * 
- * Samkith jain September 6, 2024
- * Comp Sci 3SH3, Fall 2024
- * Reference:  hello.c Operating System Concepts - 10th Edition
- * Copyright John Wiley & Sons - 2018
+ * Distribution: Ubuntu 25.10
+ * Kernel version: 6.17.0-5-generic
+ *
  */
 
 #include <linux/init.h>
@@ -14,15 +13,16 @@
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
 #include <linux/uaccess.h>
-//add jiffies file
+//Include jiffies header: To receive the extern global variable jiffies
 #include <linux/jiffies.h>
 
+#define BUFFER_SIZE 128 //Kept as buffer size for simplicity
 
-#define BUFFER_SIZE 128
-
+//Updated proc_name macro for use in init and exit functions
 #define PROC_NAME "seconds"
 
-long timeAdded;
+//Global variable for jiffies when module is added: Updated only once when proc_init() is called
+unsigned long jiffiesAdded;
 /**
  * Function prototypes
  */
@@ -36,11 +36,11 @@ static const struct proc_ops my_proc_ops = {
 int proc_init(void)
 {
 
-        // creates the /proc/hello entry
-        // the following function call is a wrapper for
+        // creates the /proc/seconds entry
+        // the following function call is a wrapper forthe proc_create() function that initialized module load into kernel
         // proc_create_data() passing NULL as the last argument
         proc_create(PROC_NAME, 0, NULL, &my_proc_ops);
-        timeAdded = jiffies;
+        jiffiesAdded = jiffies; //When loaded, global variable updated at time of function being called to save jiffies at time module added to kernel
         printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
 
 	return 0;
@@ -49,7 +49,7 @@ int proc_init(void)
 /* This function is called when the module is removed. */
 void proc_exit(void) {
 
-        // removes the /proc/hello entry
+        // removes the /proc/seconds entry
         remove_proc_entry(PROC_NAME, NULL);
 
         printk(KERN_INFO "/proc/%s removed\n", PROC_NAME);
@@ -57,6 +57,9 @@ void proc_exit(void) {
 
 /**
  * This function is called each time the /proc/seconds is read.
+ *
+ *
+ *
  * 
  * This function is called repeatedly until it returns 0, so
  * there must be logic that ensures it ultimately returns 0
@@ -74,26 +77,27 @@ ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t 
 {
         int rv = 0;
         char buffer[BUFFER_SIZE];
-        static int completed = 0;
+        static int completed = 0; //static completed - persists with each function call
 
-        if (completed) {
+
+        if (completed) { //if completed = 1, reset and return as 0
                 completed = 0;
                 return 0;
         }
 
-        completed = 1;
+        completed = 1; //if completed = 0, set and continue proc call
 
-        //retreive the proc info
-
+        //Calculation for time elapsed using jiffies and HZ rate the timer interrupt runs on
         long now = jiffies;
         long elapsedJiffies = now - timeAdded;
 
-        //conversion of jiffies to time
+        //Conversion of elapsed jiffies into seconds via HZ machine variable
         long elapsedSeconds = elapsedJiffies/HZ;
 
+        //Formats value into string for later printing to console user space
         rv = sprintf(buffer, "%lu\n", elapsedSeconds);
 
-        // copies the contents of buffer to userspace usr_buf
+        //Copies the contents of buffer to userspace usr_buf
         copy_to_user(usr_buf, buffer, rv);
 
         return rv;
